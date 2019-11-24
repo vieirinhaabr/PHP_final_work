28026ab99955a0fbeeb7a7350989f1ba7a73ac7c
Wandreus Caetano Vieira <wandreusvieira@gmail.com>
2019-11-24 02:58:48 -0300

    drag and drop

---
+++ upload.php
@@ -0,0 +1,126 @@
+<?php
+	ob_start();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro</title>
+
+    <link href="static/css/fonts.css" rel="stylesheet">
+    <link href="static/css/materialize.css" type="text/css" rel="stylesheet"/>
+    <link href="static/css/style_loading.css" rel="stylesheet">
+    <link href="static/css/bootstrap-table.min.css" rel="stylesheet">
+    <link href="static/css/bootstrap-table-materialize.min.css" rel="stylesheet">
+    <link href="static/css/bootstrap.css" rel="stylesheet">
+    <link href="static/css/bootstrap-grid.css" rel="stylesheet">
+    <link href="static/css/bootstrap-reboot.css" rel="stylesheet">
+    <link href="static/css/dropzone.css" rel="stylesheet">
+    <link href="static/css/style.css" rel="stylesheet">
+    <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+    <body class="body-after-login">
+        <div style="position: relative; padding-bottom: 15%; padding-top: 5%;" class="d-flex flex-column">
+            <div style="padding-bottom: 4%;">
+                <div>
+                    <div style="padding-bottom:2%;">
+                        <h1 style="color:white;" class="border-bottom"> UPLOAD </h1>
+                    </div>
+                    <p style="color:white;">Faça upload de suas imagens para o Post It</p>
+                </div>
+            </div>
+            <div style="width: 100%; height: 450px; border-style: hidden;" class="border-upload d-flex justify-content-center">
+                <div style="width: 45%; height: 350px; margin: auto; border-color: white;" class="border-upload d-flex flex-column">
+                    <div style="margin: auto;">
+                        <!-- <div class="d-flex justify-content-center">
+                            <h4 style="color: white; padding-bottom: 1px;"> Drag and Drop </h4>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <h6 style="color: white; padding-bottom: 10%;"> Arraste a imagem até este quadrado </h6>
+                        </div>
+                        <img src="https://www.pngkey.com/png/full/50-505769_gallery-gallery-white-icon-png.png" class="center" height="60px" width="70px"> -->
+                        <form action="/" method="post" class="dropzone" id="my-awesome-dropzone" enctype="multipart/form-data" action="__URL__"></form>
+                    </div>
+                </div>
+                <div style="margin: auto;">
+                    <h4 style="color: white; padding-bottom: 1px;"> - OU - </h4>
+                </div>
+                <div style="width: 30%; height: 350px; margin: auto; border-style: hidden;" class="border-upload d-flex flex-column">
+                    <div style="margin: auto;">
+                        <div class="d-flex justify-content-center">
+                            <h4 style="color: white; padding-bottom: 1px;"> Selecione </h4>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <h6 style="color: white; padding-bottom: 10%;"> Selecione o arquivo pelo explorer </h6>
+                        </div>
+                        <button id="button-login"> Escolher </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
+<script src="static/js/jquery-3.4.1.min.js"></script>
+<script src="static/js/materialize.js"></script>
+<script src="static/js/bootstrap-table.min.js"></script>
+<script src="static/js/bootstrap-table-materialize.min.js"></script>
+<script src="static/js/bootstrap-table-pt-BR.min.js"></script>
+<script src="static/js/bootstrap.bundle.js"></script>
+<script src="static/js/bootstrap.js"></script>
+<script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+<script src="static/js/dropzone.js"></script>
+<script>
+    $(function(){
+  Dropzone.options.myAwesomeDropzone = {
+    maxFilesize: 5,
+    addRemoveLinks: true,
+    dictResponseError: 'Server not Configured',
+    acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
+    init:function(){
+      var self = this;
+      // config
+      self.options.addRemoveLinks = true;
+      self.options.dictRemoveFile = "Delete";
+      //New file added
+      self.on("addedfile", function (file) {
+        console.log('new file added ', file);
+      });
+      // Send file starts
+      self.on("sending", function (file) {
+        console.log('upload started', file);
+        $('.meter').show();
+      });
+      
+      // File upload Progress
+      self.on("totaluploadprogress", function (progress) {
+        console.log("progress ", progress);
+        $('.roller').width(progress + '%');
+      });
+
+      self.on("queuecomplete", function (progress) {
+        $('.meter').delay(999).slideUp(999);
+      });
+      
+      // On removing file
+      self.on("removedfile", function (file) {
+        console.log(file);
+      });
+    }
+  };
+})
+</script>
+
+<?php
+
+$conteudo = ob_get_contents();
+
+ ob_end_clean ();
+
+include 'layout.php';
+
+?>
